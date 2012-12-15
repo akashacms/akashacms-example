38b99c4bf2fecea187e7d6aae2eace977c8b481b
David Herron <david@davidherron.com>
2012-12-14 16:13:11 -0800

    Add directory definition for partials - root domain for sitemap

--- build.js
+++ build.js
@@ -6,8 +6,11 @@ var url    = require('url');
 
 akasha.process({
     root_layouts: 'layouts',               // Directory for layout files
+    root_partials: 'partials',             // Directory for partials
     root_out: 'out',                       // Rendered output goes here
     root_docs: [ 'assets', 'documents'],   // Directory/ies of input files
+    
+    root_url: "http://example.akashacms.com", // Root URL for the site this will generate
     
     data: {
         // Any objects put here are available in templates as data
