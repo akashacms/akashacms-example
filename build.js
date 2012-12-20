b1063d8ff467b7387b20f6daca42368bd4d7151f
David Herron <david@davidherron.com>
2012-12-19 20:13:40 -0800

    Change build.js for multiple assets directory etc

--- build.js
+++ build.js
@@ -1,10 +1,11 @@
 var akasha = require('akashacms');
 
 akasha.process({
+    root_assets: [ 'boilerplate', 'assets' ],
     root_layouts: 'layouts',               // Directory for layout files
     root_partials: 'partials',             // Directory for partials
     root_out: 'out',                       // Rendered output goes here
-    root_docs: [ 'assets', 'documents'],   // Directory/ies of input files
+    root_docs: [ 'documents' ],   // Directory/ies of input files
     
     root_url: "http://example.akashacms.com", // Root URL for the site this will generate
     
