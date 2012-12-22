bae28ca6071be27e6029a36fe79195b670d2b819
David Herron <david@davidherron.com>
2012-12-22 00:18:07 -0800

    Initial revision

---
+++ config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+    root_assets: [ 'boilerplate', 'assets' ],
+    root_layouts: 'layouts',               // Directory for layout files
+    root_partials: 'partials',             // Directory for partials
+    root_out: 'out',                       // Rendered output goes here
+    root_docs: [ 'documents' ],   // Directory/ies of input files
+    
+    root_url: "http://example.akashacms.com", // Root URL for the site this will generate
+    
+    data: {
+        // Any objects put here are available in templates as data
+    },
+    
+    funcs: {
+        // Any functions put here are available in templates as functions
+    }
+
+};
