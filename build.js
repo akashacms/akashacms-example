b4878f7540cae6dda61395302b60322d0bf310dd
David Herron <david@davidherron.com>
2012-12-22 19:23:22 -0800

    Comments

--- build.js
+++ build.js
@@ -1,3 +1,7 @@
-var akasha = require('akashacms');
 
+// This is no longer necessary, because you can simply do "akashacms build"
+// However this is being kept as an example of using the akashacms API
+// in your own software.
+
+var akasha = require('akashacms');
 akasha.process(require('./config'));
