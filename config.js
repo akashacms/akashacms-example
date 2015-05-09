e033b5149e5c88483e898716eee1d5f68a376ac9
David Herron <david@davidherron.com>
2015-05-09 00:26:51 -0700

    Update for JSON formatter

--- config.js
+++ config.js
@@ -85,32 +85,6 @@ module.exports = {
             { name: 'akashacms-booknav', plugin: require('akashacms-booknav') },
             { name: 'akashacms-base', plugin: require('akashacms-base') }
         ]);
-        
-        /*
-        akasha.registerRenderChain({
-            match: function(fname) {
-                var matches;
-                if ((matches = fname.match(/^(.*\.html)(\.json)$/)) !== null) {
-                    console.log('matched .html.json '+ fname);
-                  return {
-                    path: matches[0],
-                    renderedFileName: matches[1],
-                    extension: matches[2],
-                    doLayouts: true
-                  };
-                } else {
-                  return null;
-                }
-          },
-          renderSync: function(text, metadata) {
-            console.log('renderSync .html.json '+ text);
-            return text;
-          },
-          render: function(text, metadata, done) {
-            console.log('render .html.json '+ text);
-            done(null, text );
-          }
-        });*/
     }
 
 };
