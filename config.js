513e39ce1a8f4c8b61c90dc313f3cd0681d1a50d
David Herron <david@davidherron.com>
2019-10-04 21:18:31 -0700

    Use @akashacms/plugins-tagged-content

--- config.js
+++ config.js
@@ -75,7 +75,7 @@ config
     .use(require('akashacms-external-links'))
     .use(require('akashacms-footnotes'))
     .use(require('akashacms-affiliates'))
-    .use(require('akashacms-tagged-content'), {
+    .use(require('@akashacms/plugins-tagged-content'), {
         sortBy: 'title',
         // @tagDescription@ can only appear once
         headerTemplate: "---\ntitle: @title@\nlayout: tagpage.html.ejs\n---\n<p><a href='./index.html'>Tag Index</a></p><p>Pages with tag @tagName@</p><p>@tagDescription@</p>",
